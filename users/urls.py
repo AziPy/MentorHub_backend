746b49e2274cf2761118902b59cd23fa6510055a
fedropoll <arstanbekovasil10@gmail.com>
2025-10-25 19:21:45 +0600

    added the login function

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', LoginView.as_view(), name='login'),
-
 ]
