2933134bf17672bcff0cd970f794084fa95d63bb
fedropoll <arstanbekovasil10@gmail.com>
2025-10-25 19:26:40 +0600

    added an exit function

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.db import router
 from django.urls import path, include
 
-from users.views.auth_views import RegisterView, LoginView
+from users.views.auth_views import RegisterView, LoginView, LogoutView
 from users.views.profile_views import UserProfileView
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', LoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
 ]
